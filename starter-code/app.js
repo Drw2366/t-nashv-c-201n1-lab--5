334e19564b07a6f874e29b2599bedfbc504fb6b4
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-05-15 15:49:08 -0400

    Lab5 late

--- starter-code/app.js
+++ starter-code/app.js
@@ -25,6 +25,17 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
 
 }
+// Problem 2
+function multiply(a, b) {
+    let productSum = a * b
+    let paper = 'The product of ' + a + ' and ' +  b  + ' is ' +  productSum
+    return [productSum, paper]
+  }
+  
+  console.log(multiply(5, 9));
+
+
+
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
@@ -43,6 +54,18 @@ Test this function by hand in the console to get it working, and when you think 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 }
+
+function sumAndMultiply(a,b,c) {
+    let sumOfElement = sum(a+b, c)[0];
+    let productOfElement = multiply(a+b, c)[0];
+    let additionSum = a + ' and ' + b + ' and ' + c + ' equals ' + sumOfElement
+    let multiplicationSum = 'This is the product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfElement
+    
+    return [sumOfElement, productOfElement, additionSum, multiplicationSum];
+  }
+  
+  console.log(sumAndMultiply(4,7,5));
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
